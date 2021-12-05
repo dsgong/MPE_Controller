36db02d4e5ff9f76af00a41dffdfa7ba883c5bc0
dsgong <93948094+dsgong@users.noreply.github.com>
2021-12-05 00:23:50 -0600

    Update MPE_Controller.ino

--- MPE_Controller/MPE_Controller.ino
+++ MPE_Controller/MPE_Controller.ino
@@ -1,6 +1,8 @@
 #include <MIDI.h>
 #include <stdlib.h> 
 #include "Adafruit_MPR121.h"
+#include <EEPROM.h>
+
 
 // Create and bind the MIDI interface to the default hardware Serial port
 MIDI_CREATE_DEFAULT_INSTANCE(); 
@@ -10,12 +12,16 @@ MIDI_CREATE_DEFAULT_INSTANCE();
 struct{
     int numNotes;
     byte pitchList[3];
-    int16_t pressure; //Actual midi pressure range is a 14 bit signed int
+    int16_t pressure;
 }typedef channel;
 
 channel allChannels[15];
 
-Adafruit_MPR121 cap = Adafruit_MPR121();
+Adafruit_MPR121 cap1 = Adafruit_MPR121();
+Adafruit_MPR121 cap2 = Adafruit_MPR121();
+Adafruit_MPR121 cap3 = Adafruit_MPR121();
+Adafruit_MPR121 cap4 = Adafruit_MPR121();
+int keyMinMax[48][2];
 
 void setup()
 {
@@ -44,9 +50,15 @@ void setup()
         allChannels[i].pressure = 0;
     }
 
-    if (!cap.begin(0x5A)) {
+    readMinMax(0);
+    /*if (!cap1.begin(0x5C)) {
         while (1);
-    }
+    }*/
+    cap1.begin(0x5C);
+    cap2.begin(0x5A);
+    cap3.begin(0x5D);
+    cap4.begin(0x5B);
+    //cap1 corresponds to octave 1, cap2 to octave 2 etc.
 }
 
 void handleNoteOn(byte channel, byte pitch, byte velocity){
@@ -86,11 +98,39 @@ void handleCC(byte channel, byte MSB, byte LSB){
     //Mod wheel is a CC message
 }
 
+void readMinMax(int address){
+    int temp;
+    for(int i = 0; i < 48; i++){
+        EEPROM.get(address, temp);
+        address += sizeof(int);
+        keyMinMax[i][0] = temp;
+        EEPROM.get(address, temp);
+        address += sizeof(int);
+        keyMinMax[i][1] = temp;
+    }
+}
+
 int findPressure(byte pitch){
     //returns pressure for a given pitch.
-    int val = cap.filteredData(pitch);
-    val = 153 - (0.5079 * val);
+    int val;
+    double keyMin, keyMax;
+        if(pitch < 36 || pitch > 83){
+        return 0;
+    }
+    keyMin = keyMinMax[pitch - 36][0];
+    keyMax = keyMinMax[pitch - 36][1];
+    if(pitch < 48) {
+        val = cap1.filteredData(pitch - 36);
+    }else if(pitch < 60){
+        val = cap2.filteredData(pitch - 48);
+    }else if(pitch < 72){
+        val = cap3.filteredData(pitch - 60);
+    }else{
+        val = cap4.filteredData(pitch - 72);
+    }
+    val = 127.0 * (val - keyMax) / (keyMin - keyMax);
     return val + ((127 - val) & ((127 - val) >>(sizeof(int) * 8 - 1)));
+    //Use point slope to scale value on the line containing (keyMax, 0) and (keyMin, 127)
     //returns minimum of scaled values and 127. 
 }
 
@@ -98,12 +138,19 @@ void loop() {
     for(int i = 0; i < 12; i++){
         MIDI.read();
         if(allChannels[i].numNotes != 0){
-            MIDI.sendAfterTouch(findPressure(i), i + 1);
+            MIDI.sendAfterTouch(findPressure(allChannels[i].pitchList[0]), i + 1);
         }
     }
-    /*for(uint8_t i = 0; i < 12; i++){
+    /*Serial.println();
+    Serial.println("Scaled Data:");
+    for(uint8_t i = 0; i < 12; i++){
         Serial.print(findPressure(i)); Serial.print("\t");
     }
-    Serial.println();*/
+    Serial.println();
+    Serial.println("Unscaled Data:");
+    for(uint8_t i = 0; i < 12; i++){
+        Serial.print(cap1.filteredData(i)); Serial.print("\t");
+    }
+    delay(1000);*/
     //Pressure write to serial monitor
 }
